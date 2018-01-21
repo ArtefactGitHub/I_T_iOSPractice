9a5748900fc78a7c79daa950fe0ab7cfda4dd825
Artefact <artefactgo@gmail.com>
2018-01-21 23:36:23 +0900

    シェーダー、テクスチャを扱うクラスの追加

---
+++ PracticeOpenGL/Source/Framework/Implement/GLProgramParameter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PracticeOpenGL.Source.Framework.Implement
+{
+    public class GLProgramParameter
+    {
+        public GLProgram GLProgram { get { return program; } }
+
+        public int PositionAttribute { get { return positionAttribute; } }
+        public int TextureCoordinateAttribute { get { return textureCoordinateAttribute; } }
+        public int MatrixUniform { get { return matrixUniform; } }
+        public int TextureUniform { get { return textureUniform; } }
+
+        GLProgram program;
+        int positionAttribute,
+            textureCoordinateAttribute,
+            matrixUniform,
+            textureUniform;
+
+        public void Use()
+        {
+            program.Use();
+        }
+
+        public GLProgramParameter(string vShaderFileName, string fShaderFileName)
+        {
+            program = new GLProgram(vShaderFileName, fShaderFileName);
+
+            program.AddAttribute("position");
+            program.AddAttribute("textureCoordinates");
+
+            if (!program.Link())
+            {
+                Debug.WriteLine("Link failed.");
+                Debug.WriteLine(string.Format("Program Log: {0}", program.ProgramLog()));
+                Debug.WriteLine(string.Format("Fragment Log: {0}", program.FragmentShaderLog()));
+                Debug.WriteLine(string.Format("Vertex Log: {0}", program.VertexShaderLog()));
+
+                program = null;
+                return;
+            }
+
+            positionAttribute = program.GetAttributeIndex("position");
+            textureCoordinateAttribute = program.GetAttributeIndex("textureCoordinates");
+            matrixUniform = program.GetUniformIndex("matrix");
+            textureUniform = program.GetUniformIndex("texture");
+        }
+    }
+}
---
+++ PracticeOpenGL/Source/Framework/Implement/Texture.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using CoreGraphics;
+using Foundation;
+using OpenTK.Graphics.ES20;
+using UIKit;
+
+namespace PracticeOpenGL.Source.Framework.Implement
+{
+    public class Texture
+    {
+        #region property
+
+        GLGraphics m_GLGraphics;
+
+        string m_FileName = string.Empty;
+
+        int m_TextureId = 0;
+
+        #endregion
+
+        public Texture(GLGame glGame, string fileName)
+        {
+            this.m_GLGraphics = glGame.GetGLGraphics();
+            this.m_FileName = fileName;
+
+            Load();
+        }
+
+        public void Bind()
+        {
+            GL.BindTexture(TextureTarget.Texture2D, m_TextureId);
+        }
+
+        public void UnBind()
+        {
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+        }
+
+        public void Dispose()
+        {
+            if (m_TextureId != 0)
+            {
+                Bind();
+                GL.DeleteTexture(m_TextureId);
+            }
+        }
+
+        public void Reload()
+        {
+            Load();
+        }
+
+        #region private function
+
+        void Load()
+        {
+            GL.Enable(EnableCap.Texture2D);
+            GL.Enable(EnableCap.Blend);
+
+            GL.Hint(HintTarget.GenerateMipmapHint, HintMode.Nicest);
+
+            //GL.GenTextures(1, out m_TextureId);
+            //GL.BindTexture(TextureTarget.Texture2D, m_TextureId);
+            GenAndBindTexture();
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)All.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)All.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Nearest);
+
+            //TODO Remove the Substring method if you don't support iOS versions prior to iOS 6.
+            //string extension = Path.GetExtension(m_FileName).Substring(1);
+            string extension = Path.GetExtension(m_FileName);
+            string baseFilename = Path.GetFileNameWithoutExtension(m_FileName);
+
+            string path = NSBundle.MainBundle.PathForResource(baseFilename, extension);
+            NSData texData = NSData.FromFile(path);
+
+            UIImage image = UIImage.LoadFromData(texData);
+            if (image == null)
+                return;
+
+            nint width = image.CGImage.Width;
+            nint height = image.CGImage.Height;
+
+            CGColorSpace colorSpace = CGColorSpace.CreateDeviceRGB();
+            byte[] imageData = new byte[height * width * 4];
+            CGContext context = new CGBitmapContext(imageData, width, height, 8, 4 * width, colorSpace,
+                                                      CGBitmapFlags.PremultipliedLast | CGBitmapFlags.ByteOrder32Big);
+
+            context.TranslateCTM(0, height);
+            context.ScaleCTM(1, -1);
+            colorSpace.Dispose();
+            context.ClearRect(new CGRect(0, 0, width, height));
+            context.DrawImage(new CGRect(0, 0, width, height), image.CGImage);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, (int)width, (int)height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, imageData);
+            context.Dispose();
+        }
+
+        void GenAndBindTexture()
+        {
+            if (m_TextureId == 0)
+            {
+                GL.GenTextures(1, out m_TextureId);
+                Bind();
+            }
+            else
+            {
+                Debug.WriteLine("TextureId is already used");
+            }
+        }
+
+        #endregion
+    }
+}
