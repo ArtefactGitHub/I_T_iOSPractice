bc5042ceb4cac5401c3e7ec4dd586ad74b6502fe
Artefact <artefactgo@gmail.com>
2018-03-03 11:38:44 +0900

    GLGraphics 描画領域のサイズを、bounds を返すように変更

--- PracticeOpenGL/Source/Framework/Implement/GLGraphics.cs
+++ PracticeOpenGL/Source/Framework/Implement/GLGraphics.cs
@@ -13,10 +13,20 @@ namespace PracticeOpenGL.Source.Framework
 
         public int GetWidth()
         {
-            return (int)m_GlView.DrawableWidth;
+            return (int)m_GlView.Bounds.Width;
         }
 
         public int GetHeight()
+        {
+            return (int)m_GlView.Bounds.Height;
+        }
+
+        public int GetDrawableWidth()
+        {
+            return (int)m_GlView.DrawableWidth;
+        }
+
+        public int GetDrawableHeight()
         {
             return (int)m_GlView.DrawableHeight;
         }
