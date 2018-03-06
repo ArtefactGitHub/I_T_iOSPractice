1eed4c1b8db18b41be81303563796164bba00341
Artefact <artefactgo@gmail.com>
2018-03-06 23:58:30 +0900

    描画テストの修正

--- PracticeOpenGL/Source/Workspace/GLGameTest/Screen/Draw2DTestScreen.cs
+++ PracticeOpenGL/Source/Workspace/GLGameTest/Screen/Draw2DTestScreen.cs
@@ -150,7 +150,7 @@ namespace PracticeOpenGL.Source.Workspace.GLGameTest
             // スプライトの描画
             m_SpriteBatcher.BeginBatch(m_Texture);
             {
-                m_SpriteBatcher.DrawSprite(0, 100, 128.0f, 128.0f, m_RegionUL);
+                m_SpriteBatcher.DrawSprite(0, 0, 128.0f, 128.0f, m_RegionUL);
             }
             m_SpriteBatcher.EndBatch();
 
@@ -158,7 +158,7 @@ namespace PracticeOpenGL.Source.Workspace.GLGameTest
             m_SpriteBatcher.BeginBatch(m_Texture);
             {
                 TextureRegion keyFrame = m_Animation.GetKeyFrame(m_AnimationSample.m_Time, Animation.ANIMATION_LOOPING);
-                m_SpriteBatcher.DrawSprite(300, 600, 64, 64, keyFrame);
+                m_SpriteBatcher.DrawSprite(660, 1220, 128, 128, keyFrame);
                 //m_SpriteBatcher.DrawSprite(0, 0, 128.0f, 128.0f, m_RegionUL2);
             }
             m_SpriteBatcher.EndBatch();
