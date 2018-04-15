4525b9b0829164a6b68099c86ea25e5617b7d8b6
Artefact <artefactgo@gmail.com>
2018-04-15 18:35:22 +0900

    StaticLibrary 用のプロジェクトなどの追加

---
+++ PracticeOpenGL/Native/CLib/CLib/CLib.c
@@ -0,0 +1,14 @@
+//
+//  CLib.c
+//  CLib
+//
+//  Created by Artefact on 2018/03/24.
+//  Copyright © 2018年 Artefact. All rights reserved.
+//
+
+#include "CLib.h"
+
+int32_t clib_add_internal(int32_t left, int32_t right)
+{
+    return left + right;
+}
