cd8dfd9c1068a27bec3025b422efb24371eaa6c2
Artefact <artefactgo@gmail.com>
2018-04-15 18:35:41 +0900

    バインディングプロジェクトの追加

---
+++ PracticeOpenGL/Native/BindingCLib/BindingCLib/ApiDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace com.Artefact.BindingCLib
+{
+    static class CFunctions
+    {
+        // extern int32_t clib_add_internal (int32_t left, int32_t right);
+        [DllImport("__Internal")]
+        static extern int clib_add_internal(int left, int right);
+    }
+}
